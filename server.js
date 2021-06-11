5d970bf56e34476947ea435ad9b42b867b7297dc
ProfMary <profmaryagoyi@gmail.com>
2021-06-11 20:34:47 +0300

    makeit better

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 const express =require('express');
 const app =express();
 const PORT =process.env.PORT || 5000;
-const bodyParser=require('body-parser');
-const request=require('request');
 
 app.set("view engine", 'ejs');
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({extended:false}))
-app.use(bodyParser.json());
+
 app.get("/", (req, res)=>{
     res.render('index', {title:"ABC University"});
 });
