b0728f54b5a2ea78dc7ab3f34faf26d7ed57439d
ProfMary <profmaryagoyi@gmail.com>
2021-06-11 19:51:24 +0300

    makeit better

--- server.js
+++ server.js
@@ -8,6 +8,26 @@ app.use(express.static('public'));
 app.get("/", (req, res)=>{
     res.render('index', {title:"ABC University"});
 });
+app.get('/aboutus', (req, res)=>{
+    res.render("about", {title:"ABC Univerity"});
+})
+
+app.get('/dormitory', (req, res)=>{
+    
+    res.render("dormitory", {title:"ABC Dormitory"});
+})
+app.get('/transport', (req, res)=>{
+    res.render("transport", {title:"ABC Transport"});
+})
+app.get('/library', (req, res)=>{
+    res.render("library", {title:"ABC Library"});
+})
+app.get('/cafeteria', (req, res)=>{
+    res.render("cafeteria", {title:"ABC Cafeteria"});
+})
+app.get('/recreation', (req, res)=>{
+    res.render("recreation", {title:"ABC Recreation"});
+})
 
 
 app.listen(PORT, ()=>{
